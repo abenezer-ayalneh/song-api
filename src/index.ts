afade8906432630690fa58b7b5f90a5e4e3051be
abenezer-ayalneh <abenezer.ayalneh.42@gmail.com>
2024-02-03 19:49:49 +0300

    feat: implement delete endpoint

--- src/index.ts
+++ src/index.ts
@@ -35,7 +35,7 @@ app.get('/list', async (req: Request, res: Response) => {
   res.status(200).json(songs)
 })
 
-// Update songs
+// Update song
 app.patch('/update/:id', async (req: Request, res: Response) => {
   const { id } = req.params
   const { title, artist, album, genre } = req.body
@@ -44,6 +44,16 @@ app.patch('/update/:id', async (req: Request, res: Response) => {
   res.status(200).json(song)
 })
 
+
+// delete song
+app.delete('/delete/:id', async (req: Request, res: Response) => {
+  const { id } = req.params
+  await Song.findByIdAndDelete(id).exec()
+
+  res.status(200).send('Deleted successfully')
+})
+
+// Start listening
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`)
 })

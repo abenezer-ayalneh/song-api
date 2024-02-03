9e29c38f0e32050fc35285141c2c4ed618407636
abenezer-ayalneh <abenezer.ayalneh.42@gmail.com>
2024-02-03 11:26:44 +0300

    chore: working on authenticating to mongo db with user and password

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,33 @@
-import express, { Express, Request, Response , Application } from 'express';
+import express, {Application, Request, Response} from 'express';
 import dotenv from 'dotenv';
+import {Song} from "./utils/db/mongoose";
 
 //For env File
-dotenv.config();
+dotenv.config()
 
-const app: Application = express();
-const port = process.env.PORT || 8000;
+const app: Application = express()
+const port = process.env.PORT || 3000
+app.use(express.json())
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Welcome to Express & TypeScript Server');
-});
+  res.send('Hello World!')
+})
+
+app.post('/create', async (req: Request, res: Response) => {
+  // console.log({body: req.body})
+  const { title, artist, album, genre } = req.body
+
+  const song = new Song({
+    title,
+    artist,
+    album,
+    genre,
+  })
+  await song.save()
+
+  res.send('Song created')
+})
 
 app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
+  console.log(`Server is listening on port ${port}`)
+})
---
+++ src/utils/db/mongoose.ts
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+//For env File
+dotenv.config()
+
+// Create a connection to DB
+mongoose
+  .connect(process.env.MONGO_DB_URL ?? 'mongodb://localhost:27017/song')
+  .then(() => {
+    console.log('Connected to MongoDB')
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error)
+  })
+
+// Define schema
+const Schema = mongoose.Schema
+const songSchema = new Schema({
+  title: String,
+  artist: String,
+  album: String,
+  genre: String,
+})
+
+// Create model(s)
+const Song = mongoose.model('Song', songSchema)
+
+export { Song }

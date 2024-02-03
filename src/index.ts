3c6bb65f3546e3b2b38f140b3b70cd0455da5c8b
abenezer-ayalneh <abenezer.ayalneh.42@gmail.com>
2024-02-03 19:35:57 +0300

    feat: implement update endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import express, {Application, Request, Response} from 'express';
-import dotenv from 'dotenv';
-import {Song} from "./utils/db/mongoose";
+import express, {Application, Request, Response} from 'express'
+import dotenv from 'dotenv'
+import {Song} from './utils/db/mongoose'
 
 //For env File
 dotenv.config()
@@ -33,6 +33,14 @@ app.get('/list', async (req: Request, res: Response) => {
   const songs = await Song.find().exec()
 
   res.status(200).json(songs)
+})
+
+// Update songs
+app.post('/update', async (req: Request, res: Response) => {
+  const { title, artist, album, genre } = req.body
+  const song = await Song.findOneAndUpdate({ id: req.body.id }, { title, artist, album, genre }, { new: true })
+
+  res.status(200).json(song)
 })
 
 app.listen(port, () => {
